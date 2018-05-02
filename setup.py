006a2ee95a8495f60df817df6c5aec82646cab85
ljubicicbobo <ljubicic.bobo@gmail.com>
2018-05-02 20:43:03 +0200

    da

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
         'author': 'Boban Ljubicic',
         'author_email': 'ljubicic.bobo@gmail.com',
         'version': '1.0',
-        'install_requires': ['kivy==1.10.0', 'urllib3==1.22', 'fnmatch'],
+        'install_requires': ['kivy==1.10.0', 'urllib3==1.22'],
         'packages': ['SimpleTV'],
         'name': 'SimpleTV'
 }
